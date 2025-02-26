e2405cfdbe7e75ac3e003d7554bbea9029db0bcf
cp-kamil <55584766+cp-kamil@users.noreply.github.com>
2025-02-26 09:47:16 +0100

    format

--- src/app/login-external/service/customer-login.interceptor.ts
+++ src/app/login-external/service/customer-login.interceptor.ts
@@ -1,27 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+  HttpInterceptor,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class CustomeInterceptor implements HttpInterceptor {
-
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = [{ text: "LosowyTEST" }, { text: "TESTlosowy" }];
 
-    const token = [
-      {"text": "LosowyTEST"},
-      {"text": "TESTlosowy"},
-  ]
+    localStorage.setItem("token", JSON.stringify(token));
 
-  localStorage.setItem("token", JSON.stringify(token));
-
-  let data = JSON.parse(localStorage.getItem("token")!);
+    let data = JSON.parse(localStorage.getItem("token")!);
     return next.handle(data);
   }
 }
